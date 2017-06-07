a79c9c00d3ff9094d3299b7c119a3f3130d8f94d
David Warring <david.warring@gmail.com>
2017-06-08 06:52:19 +1200

    add transforms and a little stroking color

--- lib/PDF/To/Cairo.pm
+++ lib/PDF/To/Cairo.pm
@@ -1,12 +1,12 @@
 class PDF::To::Cairo {
 
+    use Cairo;
+    use Color;
     use PDF::Content::Ops :OpCode;
     has PDF::Content::Ops $!gfx;
-    use Cairo;
     has $.content is required handles <width height>;
-    has Numeric @.ctm is rw = [ 1, 0, 0, 1, 0, 0, ]; # cairo coordinates
     has Cairo::Surface $.surface = Cairo::Image.create(Cairo::FORMAT_ARGB32, self.width, self.height);
-    has Cairo::Context $!ctx = Cairo::Context.new($!surface);
+    has Cairo::Context $.ctx = Cairo::Context.new($!surface);
 
     method TWEAK() {
         $!content.render: sub ($op, *@args, :$obj) { self.callback($op, |@args, :$obj) };
@@ -20,10 +20,33 @@ class PDF::To::Cairo {
         (x, -y);
     }
 
-    method Save() { $!ctx.save; }
-    method Restore() { $!ctx.restore; }
+    method !set-color($_) {
+        my ($cs, $colors) = .kv;
+        given $cs {
+            when 'DeviceRGB' {
+                $!ctx.rgb( |$colors );
+            }
+            when 'DeviceGray' {
+                my @rgb = (1..3).map: {$colors[0] - 1.0};
+                $!ctx.rgb( |@rgb );
+            }
+            default {
+                warn "can't handle colorspace: $_";
+            }
+        }
+    }
+    method !set-stroke-color { self!set-color($!gfx.StrokeColor) }
+    method !set-fill-color { self!set-color($!gfx.StrokeColor) }
+
+    method Save()      { $!ctx.save; }
+    method Restore()   { $!ctx.restore; }
     method ClosePath() { $!ctx.close_path; }
-    method Stroke() { $!ctx.stroke; }
+    method Stroke()    {
+        self!set-stroke-color;
+        $!ctx.stroke;
+    }
+    method SetStrokeRGB(*@) {}
+    method SetFillRGB(*@) {}
 
     method MoveTo(Numeric $x, Numeric $y) {
         $!ctx.move_to: |self!coords($x,$y);
@@ -36,7 +59,20 @@ class PDF::To::Cairo {
     method CurveToInitial(Numeric $x1, Numeric $y1, Numeric $x2, Numeric $y2) {
         my \c1 = |self!coords($x1, $y1);
         my \c2 = |self!coords($x2, $y2);
-        $!ctx.curve_to( |c1, |c2, |c2);
+        $!ctx.curve_to(|c1, |c2, |c2);
+    }
+
+    method ConcatMatrix(Num(Numeric) $scale-x, Num(Numeric) $skew-x,
+                        Num(Numeric) $skew-y, Num(Numeric) $scale-y,
+                        Num(Numeric) $trans-x, Num(Numeric) $trans-y) {
+
+        my $transform = Cairo::cairo_matrix_t.new(
+            :xx($scale-x), :yy($scale-y),
+            :yx(-$skew-x), :xy(-$skew-y),
+            :x0($trans-x), :y0(-$trans-y),
+            );
+
+        $!ctx.transform( $transform );
     }
 
     method callback($op, *@args, :$obj) {
--- t/00-basic.t
+++ t/00-basic.t
@@ -2,6 +2,7 @@ use v6;
 use Test;
 use PDF::Lite;
 use PDF::To::Cairo;
+use PDF::Content::Util::TransformMatrix;
 
 my $pdf = PDF::Lite.new;
 my $page = $pdf.add-page;
@@ -15,6 +16,51 @@ $gfx.CurveToInitial( 300, 800,  400, 720 );
 $gfx.ClosePath;
 $gfx.Stroke;
 $gfx.Restore;
+
+$gfx.Save;
+
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+
+$gfx.SetStrokeRGB(1.0,.2,.2);
+$gfx.ConcatMatrix: PDF::Content::Util::TransformMatrix::scale(1.2);
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+
+$gfx.SetStrokeRGB(.2, 1.0, .2);
+$gfx.ConcatMatrix: PDF::Content::Util::TransformMatrix::translate(8,8);
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+
+$gfx.Save;
+$gfx.SetStrokeRGB(.2, .2, 1.0);
+$gfx.ConcatMatrix: PDF::Content::Util::TransformMatrix::skew(.15);
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+$gfx.ConcatMatrix: PDF::Content::Util::TransformMatrix::skew(.15);
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+$gfx.Restore;
+
+$gfx.Save;
+$gfx.SetStrokeRGB(.7, .2, .7);
+$gfx.ConcatMatrix: PDF::Content::Util::TransformMatrix::rotate(.1);
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+$gfx.ConcatMatrix: PDF::Content::Util::TransformMatrix::rotate(.1);
+$gfx.MoveTo(50,50);
+$gfx.LineTo(50,100);
+$gfx.Stroke;
+$gfx.Restore;
+
+$gfx.Restore;
+
 $pdf.save-as: "t/00-basic.pdf";
 lives-ok {$feed.surface.write_png: "t/00-basic.png"}, 'write to png';
 done-testing;
